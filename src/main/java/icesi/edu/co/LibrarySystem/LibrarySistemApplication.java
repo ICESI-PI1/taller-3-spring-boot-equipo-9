b9ff598a1a60cfc357545929c707c81337e4dda2
Santiago Arboleda Velasco <72291029+Santi1761@users.noreply.github.com>
2023-10-10 10:27:49 -0500

    Run successfull

--- src/main/java/icesi/edu/co/LibrarySystem/LibrarySistemApplication.java
+++ src/main/java/icesi/edu/co/LibrarySystem/LibrarySistemApplication.java
@@ -2,12 +2,12 @@ package icesi.edu.co.LibrarySystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("icesi.edu.co.LibrarySystem.persistence.models")
 public class LibrarySistemApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LibrarySistemApplication.class, args);
     }
-
 }
--- src/main/java/icesi/edu/co/LibrarySystem/persistence/models/Book.java
+++ src/main/java/icesi/edu/co/LibrarySystem/persistence/models/Book.java
@@ -1,22 +1,39 @@
 package icesi.edu.co.LibrarySystem.persistence.models;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+
+
+
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Book {
     @Id
     @GeneratedValue
+
     private Long id;
     private String title;
-    private Data dataPublicher;
+    private java.util.Date dataPublisher;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id") // El nombre de la columna de la clave foránea en la tabla Book
     private Author author;
 
-    public Book(Book book) {this(book.getId(), book.getTitle(),book.getDataPublicher(), book.getAuthor());}
+
+
+
+
+    public Book(Book book) {
+        this(book.getId(), book.getTitle(),book.getDataPublisher(), book.getAuthor());
+    }
 
 }
